a0ef20ca411599403e84abf70ceb007f9b83caa0
David PHAM-VAN <dev.nfet.net@gmail.com>
2019-02-01 18:00:13 -0500

    Improve Clip Widgets

--- pdf/lib/widgets.dart
+++ pdf/lib/widgets.dart
@@ -23,6 +23,7 @@ import 'package:pdf/pdf.dart';
 import 'package:vector_math/vector_math_64.dart';
 
 part 'widgets/basic.dart';
+part 'widgets/clip.dart';
 part 'widgets/container.dart';
 part 'widgets/content.dart';
 part 'widgets/document.dart';
