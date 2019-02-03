bc6bc8e44625c2e7a522d3e951b6a81c82b50ef3
David PHAM-VAN <dev.nfet.net@gmail.com>
2019-02-03 08:00:06 -0500

    Improve printing example

--- printing/example/lib/document.dart
+++ printing/example/lib/document.dart
@@ -1,8 +1,10 @@
-import 'dart:io';
 import 'dart:async';
+
+import 'package:flutter/widgets.dart' as fw;
 
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart';
+import 'package:printing/printing.dart';
 
 const green = PdfColor.fromInt(0xff9ce5d0);
 const lightGreen = PdfColor.fromInt(0xffcdf1e7);
@@ -100,7 +102,12 @@ class Category extends StatelessWidget {
 }
 
 Future<PdfDocument> generateDocument(PdfPageFormat format) async {
-  final pdf = Document(deflate: zlib.encode);
+  final pdf = PdfDoc();
+
+  final profileImage = await pdfImageFromImageProvider(
+      pdf: pdf.document,
+      image: fw.NetworkImage(
+          "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&s=200"));
 
   pdf.addPage(MyPage(
     pageFormat: format.applyMargin(
@@ -173,11 +180,7 @@ Future<PdfDocument> generateDocument(PdfPageFormat format) async {
                         width: 100,
                         height: 100,
                         color: lightGreen,
-                        child: Shape(
-                            "m 4.7739997,16.673194 c 2.8848088,-1.051554 3.8069366,-1.938341 3.8069366,-3.837128 0,-1.140022 -0.8809362,-0.76766 -1.2675746,-2.8553186 C 7.1528085,9.114789 6.3748508,9.9669423 6.2257657,7.9897463 c 0,-0.7877872 0.4247873,-0.9836808 0.4247873,-0.9836808 0,0 -0.2157873,-1.1659998 -0.3005107,-2.0633191 C 6.2454254,3.824959 6.996,0.93617193 11.000001,0.93617193 c 4.003763,0 4.754807,2.88878707 4.649723,4.00657447 -0.08471,0.8973193 -0.300277,2.0633191 -0.300277,2.0633191 0,0 0.424551,0.1958936 0.424551,0.9836808 -0.148615,1.977192 -0.926571,1.1250427 -1.087125,1.9910011 -0.387107,2.0876586 -1.267576,1.7152966 -1.267576,2.8553186 0,1.898787 0.92166,2.785574 3.806467,3.837128 C 20.119703,17.727789 22,18.80298 22,19.536236 l 0,2.463765 -10.999999,0 -11.000001,0 0,-2.463765 C 0,18.80298 1.8800639,17.727789 4.7739997,16.673194",
-                            width: 22.0,
-                            height: 21.0,
-                            fillColor: PdfColor.fromInt(0xffefefef))))
+                        child: Image(profileImage)))
               ])
         ]),
   ));
